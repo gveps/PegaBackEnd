3bec2ce436cf893610b66eda71d530c916885a2d
mblasiak <milblasiak@gmail.com>
2019-03-23 14:43:38 +0100

    Add simple adding game method skeletron

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from backend.models import Game
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def add_game(request):
+    game = Game()
+    game.id_game = 11
+    game.id_entity = 13
+    game.title = "Test Game"
+    game.save()
+    json={
+        'result':'failure',
+        'message':'Empty Request'
+    }
+    return JsonResponse(json)
