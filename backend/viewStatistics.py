30f39b0d4d9155af673b9dae3fd90810592d101b
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-03-23 18:01:23 +0100

    statistics test

--- backend/viewStatistics.py
+++ backend/viewStatistics.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 
+from backend.models import Reservation, Game
 
 def tmp(request):
+    games = Game.objects.all()
+    reservations = Reservation.objects.all()
+    result = []
+    tmp = {'title': 'a', 'count': 0}
+
+    for ele in games:
+        count = len(Reservation.objects.get(title=ele.title))
+        tmp = {'title': ele.title, 'count': count}
+        result.append(tmp)
+    result.sort()
+    print(result)
     return HttpResponse("Hello, statistics.")
