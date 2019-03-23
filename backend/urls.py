28dc14702cdab136d759b33025008e80de9ad500
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-03-23 21:54:37 +0100

    image, calendar

--- backend/urls.py
+++ backend/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_game', views.add_game, name='add_game'),
-    path('statistic', viewStatistics.tmp, name='statistics')
+    path('statistic', viewStatistics.tmp, name='statistics'),
+    path('imageTest', viewStatistics.my_image, name='imageTest'),
+    path('calendar', viewStatistics.getReservations, name='calendar')
 ]
