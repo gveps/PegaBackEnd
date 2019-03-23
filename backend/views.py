5c7624fe618677092f50ab83cd1dbb1ba4942f8a
mblasiak <milblasiak@gmail.com>
2019-03-23 16:46:38 +0100

    Add validation to adding game

--- backend/views.py
+++ backend/views.py
@@ -9,15 +9,62 @@ from backend.models import Game
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-@login_required
+
 def add_game(request):
+    title = request.GET.get("title")
+    if title is None or title == "":
+        error_json = {
+            'result': 'failure',
+            'message': 'Invalid Name'
+        }
+        return JsonResponse(data=error_json)
+    if len(title) > 50:
+        error_json = {
+            'result': 'failure',
+            'message': 'Too long title'
+        }
+        return JsonResponse(data=error_json)
+
+    description = request.GET.get("description", "")
+    photo = request.GET.get("photo")
     game = Game()
-    game.id_game = 11
-    game.id_entity = 13
-    game.title = "Test Game"
+    game.title = title
+    game.description = description
+    game.photo = photo
     game.save()
-    json={
-        'result':'failure',
-        'message':'Empty Request'
+    valid_json = {
+        'result': 'success',
+        'message': 'Game added to the system'
     }
-    return JsonResponse(json)
+
+    return JsonResponse(valid_json)
+
+
+def add_extension(request):
+    title = request.GET.get("title")
+    if title is None or title == "":
+        error_json = {
+            'result': 'failure',
+            'message': 'Invalid Name'
+        }
+        return JsonResponse(error_json)
+    if len(title) > 50:
+        error_json = {
+            'result': 'failure',
+            'message': 'Too long title'
+        }
+        return JsonResponse(error_json)
+
+    description = request.GET.get("description", "")
+    photo = request.GET.get("photo")
+    game = Game()
+    game.title = title
+    game.description = description
+    game.photo = photo
+    game.save()
+    valid_json = {
+        'result': 'success',
+        'message': 'Game added to the system'
+    }
+
+    return JsonResponse(valid_json)
