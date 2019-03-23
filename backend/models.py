d1443e49efe94963177fd80bb0b7886780033ef7
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-03-23 16:39:42 +0100

    final model

--- backend/models.py
+++ backend/models.py
@@ -3,18 +3,22 @@ from django.db import models
 
 
 class Game(models.Model):
-    id_game = models.IntegerField()
-    id_entity = models.IntegerField(primary_key=True)
+    id_game = models.IntegerField(auto_created=True)
+    # id_entity = models.IntegerField(primary_key=True, auto_created=True)
     title = models.CharField(max_length=50)
     description = models.TextField(null=True)
     photo = models.CharField(max_length=50, null=True)
-    barcode = models.CharField(max_length=43, default=None, null=True)
+    barcode = models.CharField(max_length=43, default=None, null=True, blank=True)
     rate = models.FloatField(default=5.0)
-    id_base_game = models.IntegerField(default=None, null=True)
+    id_base_game = models.IntegerField(default=None, null=True, blank=True)
+    min_players = models.IntegerField(default=1)
+    max_players = models.IntegerField(default=None, null=True)
+    time = models.IntegerField(default=None, null=True)
+
 
 
 class Malfunction(models.Model):
-    id_game_entity = models.ForeignKey(Game, on_delete=models.CASCADE)
+    idgameentity = models.ForeignKey(Game, on_delete=models.CASCADE)
     description = models.TextField(null=True)
     data = models.DateField()
     solved = models.BooleanField(default=False)
@@ -23,7 +27,7 @@ class Malfunction(models.Model):
 class Reservation(models.Model):
     data = models.DateField()
     id_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    id_game = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
+    idgame = models.ForeignKey(Game, on_delete=models.SET_NULL, null=True)
     returned = models.BooleanField(default=False)
 
 
@@ -33,4 +37,4 @@ class CategoryType(models.Model):
 
 class GameCategory(models.Model):
     id_category = models.ForeignKey(CategoryType, on_delete=models.CASCADE)
-    id_game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    idgame = models.ForeignKey(Game, on_delete=models.CASCADE)
